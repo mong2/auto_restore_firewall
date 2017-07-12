9d0a89941cc8d3fb4be3548d0875aca3a7a8599f
Hana Lee <hlee@cloudpassage.com>
2017-07-12 09:15:33 -0700

    fixed typo

--- lib/server_controller.py
+++ lib/server_controller.py
@@ -5,8 +5,7 @@ from lib.api_session import ApiSession
 class ServerController(ApiSession):
     def __init__(self):
         super(ServerController, self).__init__()
-        self.api = cloudpassage.HttpHelper(self.session)
-        self.sanity = SanityCheck()
+        self.srv = cloudpassage.HttpHelper(self.session)
 
     def restore_firewall(self, server_id):
         endpoint = "/v2/servers/%s/commands/firewall" % server_id
